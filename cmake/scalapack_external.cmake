b5b5c29f202a63a0c31fa66d6ffbff4ac06c611a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-31 16:11:18 -0500

    cmake: autobuild scalapack 2.1.0 if missing

--- cmake/scalapack_external.cmake
+++ cmake/scalapack_external.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(Scalapack_proj
   GIT_REPOSITORY https://github.com/scivision/scalapack.git
-  GIT_TAG v2.1.0.1
+  GIT_TAG v2.1.0.2
   CMAKE_ARGS "-Darith=${arith}"
 )
 
