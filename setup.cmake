aad62ab03565c51d0d184e4a8c259018f154776a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-07-22 15:51:01 -0400

    setup.cmake

--- setup.cmake
+++ setup.cmake
@@ -24,6 +24,11 @@ if(NOT DEFINED CTEST_SITE)
   set(CTEST_SITE ${CMAKE_SYSTEM_NAME})
 endif()
 
+# parallel test--use ctest_test(PARALLEL_LEVEL ${Ncpu} as setting CTEST_PARALLEL_LEVEL has no effect
+include(ProcessorCount)
+ProcessorCount(Ncpu)
+message(STATUS "${Ncpu} CPU cores detected")
+
 # test name is Fortran compiler in FC
 # Note: ctest scripts cannot read cache variables like CMAKE_Fortran_COMPILER
 if(DEFINED ENV{FC})
@@ -75,6 +80,18 @@ ctest_configure(
   RETURN_VALUE return_code
   CAPTURE_CMAKE_ERROR cmake_err)
 
+# if it's a generator or compiler mismatch, delete cache and try again
+if(NOT cmake_err EQUAL 0)
+  file(REMOVE ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt)
+
+  ctest_configure(
+    BUILD ${CTEST_BINARY_DIRECTORY}
+    SOURCE ${CTEST_SOURCE_DIRECTORY}
+    OPTIONS "${_opts}"
+    RETURN_VALUE return_code
+    CAPTURE_CMAKE_ERROR cmake_err)
+endif()
+
 if(return_code EQUAL 0 AND cmake_err EQUAL 0)
   ctest_build(
     BUILD ${CTEST_BINARY_DIRECTORY}
@@ -92,9 +109,14 @@ if(return_code EQUAL 0 AND Nerror EQUAL 0 AND cmake_err EQUAL 0)
   BUILD ${CTEST_BINARY_DIRECTORY}
   RETURN_VALUE return_code
   CAPTURE_CMAKE_ERROR ctest_err
+  PARALLEL_LEVEL ${Ncpu}
   )
 else()
   message(STATUS "SKIP: ctest_test(): returncode: ${return_code}; CMake error code: ${cmake_err}")
 endif()
 
 ctest_submit()
+
+if(NOT (return_code EQUAL 0 AND Nerror EQUAL 0 AND cmake_err EQUAL 0 AND ctest_err EQUAL 0))
+  message(FATAL_ERROR "Build and test failed.")
+endif()
