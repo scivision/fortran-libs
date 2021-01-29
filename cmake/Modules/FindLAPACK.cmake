3e455b665276d5e4fad4e3484349428b872598d6
agiorla <agiorla@epsilon-alcen.com>
2021-01-29 10:56:35 +0100

    modified cmake files so that the intsize64 option compiles and works properly with the MKL

--- cmake/Modules/FindLAPACK.cmake
+++ cmake/Modules/FindLAPACK.cmake
@@ -51,7 +51,6 @@ COMPONENTS default to Netlib LAPACK / LapackE, otherwise:
 ``LAPACK95``
   get Lapack95 interfaces for MKL or Netlib (must also specify one of MKL, Netlib)
 
-
 Result Variables
 ^^^^^^^^^^^^^^^^
 
--- cmake/lapack.cmake
+++ cmake/lapack.cmake
@@ -1,11 +1,26 @@
 # Finds Lapack, tests, and if not found or broken, autobuild Lapack
 include(FetchContent)
 
+if(intsize64)
+  if(MKL IN_LIST LAPACK_COMPONENTS)
+    list(APPEND LAPACK_COMPONENTS MKL64)
+  else()
+    if(NOT (OpenBLAS IN_LIST LAPACK_COMPONENTS
+      OR Netlib IN_LIST LAPACK_COMPONENTS
+      OR Atlas IN_LIST LAPACK_COMPONENTS
+      OR MKL IN_LIST LAPACK_COMPONENTS))
+      if(DEFINED ENV{MKLROOT})
+        list(APPEND LAPACK_COMPONENTS MKL MKL64)
+      endif()
+    endif()
+  endif()
+endif()
+
 if(NOT lapack_external)
   if(autobuild)
-    find_package(LAPACK)
+    find_package(LAPACK COMPONENTS ${LAPACK_COMPONENTS})
   else()
-    find_package(LAPACK REQUIRED)
+    find_package(LAPACK REQUIRED COMPONENTS ${LAPACK_COMPONENTS})
   endif()
 endif()
 
