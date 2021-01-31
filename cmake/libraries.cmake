6e833d6bc952a8e864bec270b378cd5687806ca5
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-31 00:43:31 -0500

    Cmake >= 3.13

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -1,23 +1,18 @@
-# this reads libraries.json as a "single source of truth"
 if(CMAKE_VERSION VERSION_LESS 3.19)
-  # FIXME: we should eventually require CMake 3.19 for this and other stability enhancements.
+  set(lapack_git https://github.com/scivision/lapack.git)
+  set(lapack_tag v3.9.0.3)
 
-  message(STATUS "Due to CMake < 3.19, using fallback library metadata in ${CMAKE_CURRENT_LIST_FILE}")
+  set(scalapack_git https://github.com/scivision/scalapack.git)
+  set(scalapack_tag v2.1.0.13)
 
-  set(lapack_url https://github.com/scivision/lapack.git)
-  set(lapack_tag v3.9.0.2)
+else()
 
-  set(scalapack_url https://github.com/scivision/scalapack.git)
-  set(scalapack_tag v2.1.0.12)
-
-  return()
-endif()
-
-# preferred method CMake >= 3.19
+# CMake >= 3.19
 file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json _libj)
 
-string(JSON lapack_url GET ${_libj} lapack url)
+string(JSON lapack_git GET ${_libj} lapack git)
 string(JSON lapack_tag GET ${_libj} lapack tag)
 
-string(JSON scalapack_url GET ${_libj} scalapack url)
+string(JSON scalapack_git GET ${_libj} scalapack git)
 string(JSON scalapack_tag GET ${_libj} scalapack tag)
+endif()
