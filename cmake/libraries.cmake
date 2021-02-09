6d42fa858f24ff8c0227c52e666f28a7eb004cab
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-02-09 14:57:28 -0500

    scalapack v2.1.0.14: use objects to avoid compiler warnings

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -3,16 +3,16 @@ if(CMAKE_VERSION VERSION_LESS 3.19)
   set(lapack_tag v3.9.0.3)
 
   set(scalapack_git https://github.com/scivision/scalapack.git)
-  set(scalapack_tag v2.1.0.13)
+  set(scalapack_tag v2.1.0.14)
 
 else()
 
-# CMake >= 3.19
-file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json _libj)
+  # CMake >= 3.19
+  file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json _libj)
 
-string(JSON lapack_git GET ${_libj} lapack git)
-string(JSON lapack_tag GET ${_libj} lapack tag)
+  string(JSON lapack_git GET ${_libj} lapack git)
+  string(JSON lapack_tag GET ${_libj} lapack tag)
 
-string(JSON scalapack_git GET ${_libj} scalapack git)
-string(JSON scalapack_tag GET ${_libj} scalapack tag)
+  string(JSON scalapack_git GET ${_libj} scalapack git)
+  string(JSON scalapack_tag GET ${_libj} scalapack tag)
 endif()
